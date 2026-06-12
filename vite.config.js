8b5f0b5e33e177d297a9f3268a49bf8b2e3311db
sixbyton-oss <sixbyton@gmail.com>
2026-06-12 23:46:34 +0200

    Add @ alias for src folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
-})
-0
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})0
