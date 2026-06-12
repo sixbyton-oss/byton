86f6f9005fc07ec762915f2575620e84f90f20c7
Byton <sixbyton@gmail.com>
2026-06-12 14:47:03 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+console.log('ENV CHECK:', import.meta.env.VITE_TMDB_API_KEY ? 'KEY LOADED' : 'KEY IS UNDEFINED')
+window.addEventListener('error', e => console.log('JS ERROR:', e.message))
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
