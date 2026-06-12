a6a88bc591f34047b84a6c04c73dd6a0c40b5ab8
sixbyton-oss <sixbyton@gmail.com>
2026-06-12 19:58:15 +0200

    Add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { AppWrapper } from "./components/common/PageMeta.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <AppWrapper>
-    <App />
-  </AppWrapper>
-);
+class ErrorBoundary extends React.Component<any, any> {
+  state = {hasError: false, error: ''}
+  static getDerivedStateFromError(error: any) {
+    return {hasError: true, error: error.toString()}
+  }
+  render() {
+    if (this.state.hasError) {
+      return <div style={{padding:30,color:'white',background:'black',fontFamily:'monospace'}}>
+        <h2>App Crashed Here:</h2>
+        <pre>{this.state.error}</pre>
+      </div>
+    }
+    return this.props.children
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ErrorBoundary><App /></ErrorBoundary>
+  </React.StrictMode>
+)
+0
